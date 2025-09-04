00352387c791538719ff9b2724d4f71ca516b3d8
DiegoEBV <u201520327@upc.edu.pe>
2025-09-04 15:53:54 -0500

    modificaciones

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withNoHttpTransferCache } from '@angular/platform-browser';
+// import { provideClientHydration, withNoHttpTransferCache } from '@angular/platform-browser'; // Temporalmente deshabilitado
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideServiceWorker } from '@angular/service-worker';
@@ -10,7 +10,7 @@ import { provideServiceWorker } from '@angular/service-worker';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
-    provideClientHydration(withNoHttpTransferCache()), 
+    // provideClientHydration(withNoHttpTransferCache()), // Temporalmente deshabilitado para resolver NG0505
     provideAnimationsAsync(),
     provideHttpClient(withFetch()), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
